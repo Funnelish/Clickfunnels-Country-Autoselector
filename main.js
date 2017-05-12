c512d9caa2ea565dbb9a69e2c38897dfd4c72713
yassine-f <yassine-f@users.noreply.github.com>
2017-05-12 14:34:16 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+/*\\\\\\\\\\\\\\\\\\\\\\\\--YaYo--///////////////////////////
+ * \\      Copyright (c) Yassine Yousfi (Funnelish)       //
+ *  \\               All Rights Reserved.                //
+ *   \\\\\\\\\\\\\\\\\\\\----<>----///////////////////////
+ *==========================================================
+ * 	FILE         :  /Funnelish/Clickfunnels-Country-Autoselector.js
+ * 	AUTHOR       :  Yassine Y.
+ * 	DESCRIPTION  : 	
+ *
+ * 	UPDATES      :
+ *	        _CREATED : May 12, 2017	 _By : Yassine Y.
+ *          _CHANGED : May 12, 2017	 _By : Yassine Y.
+ */
+ 
+<script>
+  window.onload = function() {
+    var x = document.getElementById("input-45667");
+    var input = x.getElementsByTagName("input")[0];
+    input.value = "US";   
+  };
+</script>
